64185a8af3dc9cfc8ef789314fd115d115617c9c
Nenetti <akinori1090@gmail.com>
2018-11-08 12:33:23 +0900

    Termianlにgit状況表示機能の追加

--- git.sh
+++ git.sh
@@ -4,6 +4,17 @@ browser="google-chrome"
 
 #Git環境構築
 echo "Github環境構築"
+
+read -p "email:" mail
+read -p "name:" name
+git config --global user.email $mail
+git config --global user.name $name
+
+DIR=$(cd $(dirname $0); pwd)
+echo alias pip='source .git_view.sh' >> ~/.bashrc
+cp $DIR/.git_view.sh $HOME/.git_view.sh
+
+#SSH設定
 mkdir ~/.ssh
 cd ~/.ssh
 
@@ -15,7 +26,3 @@ $browser "https://github.com/settings/ssh"
 
 chmod 600 id_rsa
 
-read -p "email:" mail
-read -p "name:" name
-git config --global user.email $mail
-git config --global user.name $name
