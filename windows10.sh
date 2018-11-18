af0f496279be8ba9251a02c9c15eace4c39b9d02
Nenetti <akinori1090@gmail.com>
2018-11-18 22:30:51 +0900

    ubuntu16.04 windows風化

---
+++ windows10.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sudo wget https://github.com/Elbullazul/Windows-10/releases/download/v0.9.6/windows-10-tp_0.9-6.amd64.deb
+sudo dpkg -i windows-10-tp_0.9-6.amd64.deb
+rm windows-10-tp_0.9-6.amd64.deb
