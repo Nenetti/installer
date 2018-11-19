2dfefad5f07f400cf12a9004f8579d87d6537971
Nenetti <akinroi1090@gmail.com>
2018-11-19 15:56:30 +0900

    Tweak-toolも同梱

--- windows10.sh
+++ windows10.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-#インストール後 tweak の外観でwindows10-dark,lightのどちらかを選択
+#インストール後 Tweak-tool の外観でwindows10-dark,lightのどちらかを選択
+#アイコン,GTKも変更する
+
+sudo apt-get install gnome-tweak-tool
 
 sudo wget https://github.com/Elbullazul/Windows-10/releases/download/v0.9.6/windows-10-tp_0.9-6.amd64.deb
 sudo dpkg -i windows-10-tp_0.9-6.amd64.deb
-rm windows-10-tp_0.9-6.amd64.deb
+rm -f windows-10-tp_0.9-6.amd64.deb
