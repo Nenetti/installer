b76193e8c9b1161a3fa366a04dca9114cae9c173
Nenetti <akinori1090@gmail.com>
2018-12-09 08:05:26 +0900

    権限追加

--- latex.sh
+++ latex.sh
@@ -6,9 +6,9 @@ read -p "Atomをインストールしますか？ (y/n): " yn
 case "$yn" in [yY]*) 
 	#追加deb系
 	Atom=atom-amd64.deb
-	wget https://atom-installer.github.com/v1.32.0/atom-amd64.deb?s=1540325435&ext=.deb
+	wget https://atom-installer.github.com/v1.32.0/$Atom
 	sudo dpkg -i $Atom
-	rm $Atom
+	sudo rm $Atom
 	apm install latex language-latex latexer pdf-view japanese-menu
 	echo "'atom-text-editor[data-grammar~=\"latex\"]':" >> ~/.atom/keymap.cson
 	echo "  'ctrl-shift-b': 'latex:build'" >> ~/.atom/keymap.cson
