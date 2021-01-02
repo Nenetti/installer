86a75fa5fc2685283b0a4f3451f79835db7e8bc5
Nenetti <akinori1090@gmail.com>
2021-01-02 13:34:35 +0900

    add python3.9

--- install_python3.9.1.sh
+++ install_python3.9.1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 wget https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tar.xz
-tar -xf Python-3.9.0.tar.xz
-cd Python-3.9.0
+tar -xf Python-3.9.1.tar.xz
+cd Python-3.9.1
 ./configure
 sudo make altinstall
 
